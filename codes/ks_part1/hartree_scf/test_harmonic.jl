1bba838e315a0bb008b6a5d6955a3bd2745d1474
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-10-19 10:25:51 +0700

    Eigenvalues are different from Octopus

--- codes/ks_part1/hartree_scf/test_harmonic.jl
+++ codes/ks_part1/hartree_scf/test_harmonic.jl
@@ -33,17 +33,17 @@ end
 function main()
 
     Random.seed!(1234)
-    
-    AA = [0.0, 0.0, 0.0]
-    BB = [6.0, 6.0, 6.0]
+
+    AA = [-3.0, -3.0, -3.0]
+    BB = [3.0, 3.0, 3.0]
     NN = [25, 25, 25]
-    
+
     fdgrid = FD3dGrid( NN, AA, BB )
 
     # follow the potential used in Arias DFT++ tutorial
-    my_pot_harmonic( fdgrid ) = pot_harmonic( fdgrid, ω=2, center=[3.0, 3.0, 3.0] )
+    my_pot_harmonic( fdgrid ) = pot_harmonic( fdgrid, ω=2 )
 
-    Ham = Hamiltonian( fdgrid, my_pot_harmonic )
+    Ham = Hamiltonian( fdgrid, my_pot_harmonic, func_1d=build_D2_matrix_9pt )
 
     Nbasis = prod(NN)
 
@@ -54,6 +54,7 @@ function main()
     ortho_sqrt!(psi)
     psi = psi/sqrt(dVol)
 
+    println("Test normalization")
     for i in 1:Nstates
         @printf("%18.10f\n", dot(psi[:,i], psi[:,i])*dVol )
     end
@@ -62,7 +63,7 @@ function main()
     @printf("Integrated Rhoe = %18.10f\n", sum(Rhoe)*dVol)
 
     update!( Ham, Rhoe )
-    
+
     evals = zeros(Float64,Nstates)
     Etot_old = 0.0
     dEtot = 0.0
@@ -71,18 +72,21 @@ function main()
     NiterMax = 100
 
     for iterSCF in 1:NiterMax
-        
-        evals = diag_LOBPCG!( Ham, psi, Ham.precKin, verbose_last=false )
+
+        #evals = diag_LOBPCG!( Ham, psi, Ham.precKin, verbose_last=true )
+        evals = diag_Emin_PCG!( Ham, psi, Ham.precKin, verbose_last=true )
         psi = psi/sqrt(dVol)
 
         Rhoe_new = calc_rhoe( psi )
+        @printf("Integrated Rhoe_new = %18.10f\n", sum(Rhoe_new)*dVol)
 
         Rhoe = betamix*Rhoe_new + (1-betamix)*Rhoe
+        @printf("Integrated Rhoe     = %18.10f\n", sum(Rhoe)*dVol)
 
         update!( Ham, Rhoe )
-        
+
         Etot = sum( calc_energies( Ham, psi ) )
-        
+
         dRhoe = norm(Rhoe - Rhoe_new)
         dEtot = abs(Etot - Etot_old)
 
@@ -90,6 +94,11 @@ function main()
 
         if dEtot < 1e-6
             @printf("Convergence is achieved in %d iterations\n", iterSCF)
+            Hsub = Hermitian( psi' * (Ham*psi) )*dVol
+            evals = eigvals(Hsub)
+            for i in 1:Nstates
+                @printf("%3d %18.10f\n", i, evals[i])
+            end
             break
         end
 
@@ -98,6 +107,3 @@ function main()
 end
 
 main()
-
-
-
