0c7fc29d567e7cf12d7938e466ebd011e0f376de
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-10-19 14:13:37 +0700

    Experiment with various Gaussian potentials

---
+++ codes/ks_part1/ks_scf/test_KS_gaussian.jl
@@ -0,0 +1,129 @@
+using Printf
+using LinearAlgebra
+using SparseArrays
+using IterativeSolvers
+using IncompleteLU
+using AlgebraicMultigrid
+using Random
+
+include("../../3d/FD3dGrid.jl")
+include("../../3d/build_nabla2_matrix.jl")
+include("../../diag_Emin_PCG.jl")
+include("../../diag_davidson.jl")
+include("../../diag_LOBPCG.jl")
+include("../../ortho_sqrt.jl")
+include("../../supporting_functions.jl")
+include("../../3d_poisson/Poisson_solve_PCG.jl")
+
+include("Electrons.jl")
+include("Energies.jl")
+include("calc_rhoe.jl")
+include("Hamiltonian.jl")
+
+include("../LDA_VWN.jl")
+
+include("calc_energies.jl")
+
+function pot_gaussian( fdgrid::FD3dGrid; A=1.0, α=1.0, center=[0.0, 0.0, 0.0], normalized=false )
+    Npoints = fdgrid.Npoints
+    Vpot = zeros(Npoints)
+    if normalized
+        NN = 1.0/sqrt(α/pi)^3
+    else
+        NN = 1.0
+    end
+    for i in 1:Npoints
+        x = fdgrid.r[1,i] - center[1]
+        y = fdgrid.r[2,i] - center[2]
+        z = fdgrid.r[3,i] - center[3]
+        r2 = x^2 + y^2 + z^2
+        Vpot[i] = -A*exp( -α*r2 )/NN
+    end
+    return Vpot
+end
+
+function main()
+    Random.seed!(1234)
+
+    AA = -3.0*ones(3)
+    BB =  3.0*ones(3)
+    NN = [25, 25, 25]
+
+    fdgrid = FD3dGrid( NN, AA, BB )
+
+    println("hx = ", fdgrid.hx)
+    println("hy = ", fdgrid.hy)
+    println("hz = ", fdgrid.hz)
+    println("dVol = ", fdgrid.dVol)
+    println(fdgrid.hx*fdgrid.hy*fdgrid.hz)
+
+    my_pot_local( fdgrid ) = pot_gaussian( fdgrid, α=1.0, A=1.0, normalized=false )
+
+    Nstates = 1
+    Nelectrons = 2*Nstates
+    Ham = Hamiltonian( fdgrid, my_pot_local, Nelectrons=Nelectrons, func_1d=build_D2_matrix_9pt )
+
+    Nbasis = prod(NN)
+
+    dVol = fdgrid.dVol
+
+    psi = rand(Float64,Nbasis,Nstates)
+    ortho_sqrt!(psi)
+    psi = psi/sqrt(dVol)
+
+    for i in 1:Nstates
+        @printf("%18.10f\n", dot(psi[:,i], psi[:,i])*dVol )
+    end
+
+    Rhoe = calc_rhoe( psi )
+    @printf("Integrated Rhoe = %18.10f\n", sum(Rhoe)*dVol)
+
+    update!( Ham, Rhoe )
+
+    evals = zeros(Float64,Nstates)
+    Etot_old = 0.0
+    dEtot = 0.0
+    betamix = 0.5
+    dRhoe = 0.0
+    NiterMax = 100
+
+    for iterSCF in 1:NiterMax
+
+        evals = diag_LOBPCG!( Ham, psi, Ham.precKin, verbose_last=false )
+
+        #psi = psi*sqrt(dVol) # for diag_davidson
+        #evals = diag_davidson!( Ham, psi, Ham.precKin, verbose_last=false )
+
+        psi = psi/sqrt(dVol) # renormalize
+
+        Rhoe_new = calc_rhoe( psi )
+
+        Rhoe = betamix*Rhoe_new + (1-betamix)*Rhoe
+
+        update!( Ham, Rhoe )
+
+        calc_energies!( Ham, psi )
+        Etot = sum( Ham.energies )
+
+        dRhoe = norm(Rhoe - Rhoe_new)
+        dEtot = abs(Etot - Etot_old)
+
+        @printf("%5d %18.10f %18.10e %18.10e\n", iterSCF, Etot, dEtot, dRhoe)
+
+        if dEtot < 1e-6
+            @printf("Convergence is achieved in %d iterations\n", iterSCF)
+            @printf("\nEigenvalues:\n")
+            for i in 1:Nstates
+                @printf("%3d %18.10f\n", i, evals[i])
+            end
+            break
+        end
+
+        Etot_old = Etot
+    end
+
+    println(Ham.energies)
+
+end
+
+@time main()
