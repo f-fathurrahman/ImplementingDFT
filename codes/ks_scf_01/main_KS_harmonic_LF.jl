65cb075c6fa9dfdae75c2ec01965991d3433c8a9
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-05-21 11:48:58 +0700

    Test H atom and KS harmonic with LF c

---
+++ codes/ks_scf_01/main_KS_harmonic_LF.jl
@@ -0,0 +1,106 @@
+push!(LOAD_PATH, pwd())
+
+using Printf
+using Random
+using LinearAlgebra
+using SpecialFunctions
+
+using MyModule
+
+function pot_harmonic( grid; ω=1.0, center=[0.0, 0.0, 0.0] )
+    Npoints = grid.Npoints
+    Vpot = zeros(Npoints)
+    for i in 1:Npoints
+        x = grid.r[1,i] - center[1]
+        y = grid.r[2,i] - center[2]
+        z = grid.r[3,i] - center[3]
+        Vpot[i] = 0.5 * ω^2 *( x^2 + y^2 + z^2 )
+    end
+    return Vpot
+end
+
+function main()
+    Random.seed!(1234)
+
+    AA = [-3.0, -3.0, -3.0]
+    BB = [3.0, 3.0, 3.0]
+    NN = [25, 25, 25]
+
+    grid = LF3dGrid( NN, AA, BB )
+
+    println("hx = ", grid.hx)
+    println("hy = ", grid.hy)
+    println("hz = ", grid.hz)
+    println("dVol = ", grid.dVol)
+    println(grid.hx*grid.hy*grid.hz)
+
+    my_pot_harmonic( grid ) = pot_harmonic( grid, ω=2 )
+
+    Nstates = 4
+    Nelectrons = 2*Nstates
+    Ham = Hamiltonian( grid, my_pot_harmonic, Nelectrons=Nelectrons )
+
+    Nbasis = prod(NN)
+
+    dVol = grid.dVol
+
+    psi = rand(Float64,Nbasis,Nstates)
+    ortho_sqrt!(psi)
+    psi = psi/sqrt(dVol)
+
+    for i in 1:Nstates
+        @printf("%18.10f\n", dot(psi[:,i], psi[:,i])*dVol )
+    end
+
+    Rhoe = calc_rhoe( Ham, psi )
+    @printf("Integrated Rhoe = %18.10f\n", sum(Rhoe)*dVol)
+
+    update!( Ham, Rhoe )
+
+    evals = zeros(Float64,Nstates)
+    Etot_old = 0.0
+    dEtot = 0.0
+    betamix = 0.8
+    dRhoe = 0.0
+    NiterMax = 100
+
+    for iterSCF in 1:NiterMax
+
+        evals = diag_LOBPCG!( Ham, psi, Ham.precKin, verbose_last=false )
+
+        #psi = psi*sqrt(dVol) # for diag_davidson
+        #evals = diag_davidson!( Ham, psi, Ham.precKin, verbose_last=false )
+
+        psi = psi/sqrt(dVol) # renormalize
+
+        Rhoe_new = calc_rhoe( Ham, psi )
+
+        Rhoe = betamix*Rhoe_new + (1-betamix)*Rhoe
+
+        update!( Ham, Rhoe )
+
+        calc_energies!( Ham, psi )
+        Etot = sum( Ham.energies )
+
+        dRhoe = norm(Rhoe - Rhoe_new)
+        dEtot = abs(Etot - Etot_old)
+
+        @printf("%5d %18.10f %18.10e %18.10e\n", iterSCF, Etot, dEtot, dRhoe)
+
+        if dEtot < 1e-6
+            @printf("Convergence is achieved in %d iterations\n", iterSCF)
+            @printf("\nEigenvalues:\n")
+            for i in 1:Nstates
+                @printf("%3d %18.10f\n", i, evals[i])
+            end
+            break
+        end
+
+        Etot_old = Etot
+    end
+
+    println(Ham.energies)
+
+end
+
+@time main()
