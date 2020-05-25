a049d1828051646f83500f6e6a0f2f576b55bbb4
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-05-25 08:51:11 +0700

    Using dVol

--- codes/common/ortho_sqrt.jl
+++ codes/common/ortho_sqrt.jl
@@ -7,3 +7,13 @@ function ortho_sqrt!( psi )
     Udagger = inv(sqrt(psi'*psi))
     psi[:,:] = psi*Udagger
 end
+
+function ortho_sqrt( psi, dVol )
+    Udagger = inv(sqrt(psi'*psi*dVol))
+    return psi*Udagger
+end
+
+function ortho_sqrt!( psi, dVol )
+    Udagger = inv(sqrt(psi'*psi*dVol))
+    psi[:,:] = psi*Udagger
+end
