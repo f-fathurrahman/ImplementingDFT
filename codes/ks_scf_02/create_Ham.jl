f596dbaed4c5943ada1373fabfc3827898b2c3dd
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-05-26 13:05:27 +0700

    Fix create_Ham, test_SCF

--- codes/ks_scf_02/create_Ham.jl
+++ codes/ks_scf_02/create_Ham.jl
@@ -1,20 +1,20 @@
 function create_Ham_LiH( N::Int64 )
-    atoms = Atoms( xyz_file="LiH.xyz" )
-    pspfiles = [ joinpath("H-q1.gth"),
-                 joinpath("Li-q1.gth") ]
+    atoms = Atoms( xyz_file=joinpath(DIR_STRUCTURES,"LiH.xyz") )
+    pspfiles = [ joinpath(DIR_PSP,"H-q1.gth"),
+                 joinpath(DIR_PSP,"Li-q1.gth") ]
     AA = -8.0*ones(3)
     BB =  8.0*ones(3)
     NN = [41, 41, 41]
     #grid = FD3dGrid( NN, AA, BB )
     #grid = LF3dGrid( NN, AA, BB )
-    grid = LF3dGrid( NN, AA, BB, type_x=:sinc, type_y=:sinc, type_z=:sinc)
+    grid = LF3dGrid( NN, AA, BB, types=(:sinc,:sinc,:sinc) )
     return Hamiltonian( atoms, pspfiles, grid )    
 end
 
 function create_Ham_H2O( N::Int64 )
-    atoms = Atoms( xyz_file="H2O.xyz" )
+    atoms = Atoms( xyz_file=joinpath(DIR_STRUCTURES,"H2O.xyz") )
     pspfiles = [ joinpath(DIR_PSP,"O-q6.gth"),
-                 joinpath(DIR_PSP, "H-q1.gth") ]
+                 joinpath(DIR_PSP,"H-q1.gth") ]
     AA = -8.0*ones(3)
     BB =  8.0*ones(3)
     NN = [N,N,N]
@@ -38,8 +38,9 @@ function create_Ham_H( N::Int64 )
 end
 
 function create_Ham_NH3( N::Int64 )
-    atoms = Atoms( xyz_file="NH3.xyz" )
-    pspfiles = ["N-q5.gth", "H-q1.gth"]
+    atoms = Atoms( xyz_file=joinpath(DIR_STRUCTURES,"NH3.xyz") )
+    pspfiles = [ joinpath(DIR_PSP,"N-q5.gth"),
+                 joinpath(DIR_PSP,"H-q1.gth") ]
     AA = -8.0*ones(3)
     BB =  8.0*ones(3)
     NN = [N,N,N]
@@ -50,7 +51,8 @@ end
 
 function create_Ham_CH4( N::Int64 )
     atoms = Atoms( xyz_file="CH4.xyz" )
-    pspfiles = ["C-q4.gth", "H-q1.gth"]
+    pspfiles = [ joinpath(DIR_PSP,"C-q4.gth"),
+                 joinpath(DIR_PSP,"H-q1.gth") ]
     AA = -8.0*ones(3)
     BB =  8.0*ones(3)
     NN = [N,N,N]
@@ -65,7 +67,7 @@ function create_Ham_Ar( N::Int64 )
 
         Ar  0.0  0.0  0.0
         """ )
-    pspfiles = [ joinpath(DIR_PSP, "Ar-q8.gth") ]
+    pspfiles = [ joinpath(DIR_PSP,"Ar-q8.gth") ]
     AA = -8.0*ones(3)
     BB =  8.0*ones(3)
     NN = [N,N,N]
@@ -81,7 +83,7 @@ function create_Ham_Ne( N::Int64 )
 
         Ne  0.0  0.0  0.0
         """ )
-    pspfiles = ["Ne-q8.gth"]
+    pspfiles = [joinpath(DIR_PSP,"Ne-q8.gth")]
     AA = -8.0*ones(3)
     BB =  8.0*ones(3)
     NN = [N,N,N]
@@ -92,7 +94,8 @@ end
 
 function create_Ham_SiH4(N::Int64)
     atoms = Atoms( xyz_file="SiH4.xyz" )
-    pspfiles = ["Si-q4.gth", "H-q1.gth"]
+    pspfiles = [ joinpath(DIR_PSP,"Si-q4.gth"),
+                 joinpath(DIR_PSP,"H-q1.gth") ]
     AA = -8.0*ones(3)
     BB =  8.0*ones(3)
     NN = [N,N,N]
---
+++ codes/ks_scf_02/test_SCF.jl
@@ -0,0 +1,35 @@
+push!(LOAD_PATH, pwd())
+
+using Printf
+using Random
+using LinearAlgebra
+using SpecialFunctions
+
+using MyModule
+
+const DIR_PSP = "../pseudopotentials/pade_gth/"
+const DIR_STRUCTURES = "../structures"
+
+include("create_Ham.jl")
+
+include("KS_solve_SCF.jl")
+
+function main()
+    
+    #Ham = create_Ham_H2O(41)
+    #Ham = create_Ham_H(41)
+    #Ham = create_Ham_Ne(41)
+    Ham = create_Ham_LiH(41)
+
+    Nbasis = Ham.grid.Npoints
+    Nstates = Ham.electrons.Nstates
+    dVol = Ham.grid.dVol
+
+    psi = rand(Float64,Nbasis,Nstates)
+    ortho_sqrt!(psi,dVol)
+
+    KS_solve_SCF!(Ham, psi)
+
+end
+
+main()
