7c87b456316a767116f1376f20fcbf5906d2a8bd
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-05-29 09:35:02 +0700

    Backup plot

---
+++ codes/sch_2d/main_harmonic_plot.jl
@@ -0,0 +1,75 @@
+using Printf
+using LinearAlgebra
+using SparseArrays
+using IncompleteLU
+using Random
+
+import PyPlot
+const plt = PyPlot
+
+include("INC_sch_2d.jl")
+
+function pot_harmonic( grid; ω=1.0 )
+    Npoints = grid.Npoints
+    Vpot = zeros(Npoints)
+    for i in 1:Npoints
+        x = grid.r[1,i]
+        y = grid.r[2,i]
+        Vpot[i] = 0.5 * ω^2 *( x^2 + y^2 )
+    end
+    return Vpot
+end
+
+function main()
+
+    Random.seed!(1234)
+
+    Nx = 50
+    Ny = 50
+    grid = FD2dGrid( (-5.0,5.0), Nx, (-5.0,5.0), Ny )
+
+    ∇2 = build_nabla2_matrix( grid, stencil_order=11 )
+
+    Vpot = pot_harmonic( grid )
+    
+    Ham = -0.5*∇2 + spdiagm( 0 => Vpot )
+
+    # may choose between these two
+    #prec = ilu(-0.5*∇2)
+    prec = ilu(Ham) # this should result in faster convergence
+
+    Nstates = 10
+    Npoints = Nx*Ny
+    X = rand(Float64, Npoints, Nstates)
+    ortho_sqrt!(X)
+    #evals = diag_Emin_PCG!( Ham, X, prec, verbose=true )
+    evals = diag_LOBPCG!( Ham, X, prec, verbose=true )
+    X = X/sqrt(grid.dA) # renormalize
+
+    @printf("\n\nEigenvalues\n")
+    for i in 1:Nstates
+        @printf("%5d %18.10f\n", i, evals[i])
+    end
+
+    @printf("Plotting wave functions and slice of rhoe:\n")
+    for i in 1:Nstates
+        #
+        plt.clf()
+        plt.surf(grid.x, grid.y, reshape(X[:,i], grid.Nx, grid.Ny), cmap=:jet)
+        plt.tight_layout()
+        plt.savefig("IMG_harmonic_psi_"*string(i)*".pdf")
+        #
+        ρ = X[:,i].*X[:,i]
+        plt.clf()
+        plt.contourf(grid.x, grid.y, reshape(ρ, grid.Nx, grid.Ny), cmap=:jet)
+        plt.axis("equal")
+        plt.tight_layout()
+        plt.savefig("IMG_harmonic_rho_"*string(i)*".png", dpi=150)
+    end
+
+end
+
+main()
+
+
+
