c3fd305284836c1277175b45a7792e6d4f8b0c30
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-06-21 10:28:02 +0700

    Add E_NN

--- codes/hartree_scf/calc_energies.jl
+++ codes/hartree_scf/calc_energies.jl
@@ -1,3 +1,19 @@
+function calc_E_NN( Zvals::Array{Float64,1}, r::Array{Float64,2} )
+    Natoms = length(Zvals)
+    @assert Natoms == size(r,2)
+    E_NN = 0.0
+    for ia in 1:Natoms
+        for ja in ia+1:Natoms
+            dx = r[1,ja] - r[1,ia]
+            dy = r[2,ja] - r[2,ia]
+            dz = r[3,ja] - r[3,ia]
+            r_ij = sqrt(dx^2 + dy^2 + dz^2)
+            E_NN = E_NN + Zvals[ia]*Zvals[ja]/r_ij
+        end
+    end
+    return E_NN
+end
+
 function calc_E_kin( Ham, psi::Array{Float64,2} )
     Nbasis = size(psi,1)
     Nstates = size(psi,2)
