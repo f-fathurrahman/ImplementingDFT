2d5b19ab1e59ea8b1a8bf7b5338d276d44a1a15d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-06-22 17:04:17 +0700

    Add H and H2 with PBC

--- codes/common/potential_H_atom.jl
+++ codes/common/potential_H_atom.jl
@@ -1,10 +1,10 @@
 function pot_H_atom( atoms::Atoms, grid )
+    @assert atoms.pbc == (false,false,false)
+    @assert atoms.Nspecies == 1
     Npoints = grid.Npoints
     Vpot = zeros(Npoints)
-    
     Natoms = atoms.Natoms
     atpos = atoms.positions
-
     for ia in 1:Natoms, ip in 1:Npoints
         dx = grid.r[1,ip] - atpos[1,ia]
         dy = grid.r[2,ip] - atpos[2,ia]
@@ -14,10 +14,43 @@ function pot_H_atom( atoms::Atoms, grid )
     return Vpot
 end
 
-function pot_Hps_HGH( atoms::Atoms, grid )
+function pot_H_atom_G( atoms::Atoms, grid, gvec::GVectors )
+    @assert atoms.pbc == (true,true,true)
+    @assert atoms.Nspecies == 1
+    Npoints = grid.Npoints
+    CellVolume = grid.Lx * grid.Ly * grid.Lz
+    Nx = grid.Nx; Ny = grid.Ny; Nz = grid.Nz
+    G2 = gvec.G2
+    Ng = length(G2)
+    if typeof(grid) == LF3dGrid
+        # periodic LF
+        shifts = [0.5*grid.hx, 0.5*grid.hy, 0.5*grid.hz]
+        strf = calc_strfact_shifted( atoms, gvec, shifts )
+    else
+        # periodic FD    
+        strf = calc_strfact( atoms, gvec )
+    end
+    Vpot = zeros(Float64, Npoints)
+    Vg = zeros(ComplexF64, Nx,Ny,Nz)
+    isp = 1
+    Zval = 1.0
+    prefactor = -4*pi*Zval
+    for ig = 2:Ng
+        Vg[ig] = strf[ig,isp] * prefactor/G2[ig]
+    end
+    #
+    ifft!(Vg)
+    @views Vpot[:] = Vpot[:] + real( Vg[:] ) * Npoints / CellVolume
+    return Vpot
+end
+
+
+
+function pot_Hps_GTH( atoms::Atoms, grid )
+    @assert atoms.pbc == (false,false,false)
+    @assert atoms.Nspecies == 1    
     Npoints = grid.Npoints
     Vpot = zeros( Float64, Npoints )
-
     # Parameters
     Zval = 1
     rloc = 0.2
@@ -40,5 +73,48 @@ function pot_Hps_HGH( atoms::Atoms, grid )
                      (C1 + C2*rrloc^2)*exp(-0.5*(rrloc)^2)
         end
     end
+    return Vpot
+end
+
+function pot_Hps_GTH_G( atoms::Atoms, grid, gvec::GVectors )
+    @assert atoms.pbc == (true,true,true)
+    @assert atoms.Nspecies == 1
+    Npoints = grid.Npoints
+    CellVolume = grid.Lx * grid.Ly * grid.Lz
+    Nx = grid.Nx; Ny = grid.Ny; Nz = grid.Nz
+    G2 = gvec.G2
+    Ng = length(G2)
+    if typeof(grid) == LF3dGrid
+        # periodic LF
+        shifts = [0.5*grid.hx, 0.5*grid.hy, 0.5*grid.hz]
+        strf = calc_strfact_shifted( atoms, gvec, shifts )
+    else
+        # periodic FD    
+        strf = calc_strfact( atoms, gvec )
+    end
+    Vpot = zeros(Float64, Npoints)
+    Vg = zeros(ComplexF64, Nx,Ny,Nz)
+    isp = 1
+
+
+    # Parameters
+    Zval = 1
+    rloc = 0.2
+    C1 = -4.0663326
+    C2 = 0.6678322
+    pre1 = -4*pi*Zval
+    pre2 = sqrt(8*pi^3)*rloc^3
+
+    # Limiting case G=(0,0,0)
+    Vg[1] = 2*pi*Zval*rloc^2 + (2*pi)^1.5 * rloc^3 * (C1 + 3.0*C2)
+    Vg[1] = strf[1,isp] * Vg[1]
+    for ig in 2:Ng
+        Gr = sqrt(G2[ig])*rloc
+        expGr2 = exp(-0.5*Gr^2)
+        Vg[ig] = pre1/G2[ig]*expGr2 + pre2*expGr2 * (C1 + C2*(3.0 - Gr^2))
+        Vg[ig] = strf[ig,isp] * Vg[ig]
+    end
+    ifft!(Vg)
+    @views Vpot[:] = Vpot[:] + real( Vg[:] ) * Npoints / CellVolume
     return Vpot
 end
---
+++ codes/hartree_scf/main_atom_H_periodic.jl
@@ -0,0 +1,100 @@
+push!(LOAD_PATH, pwd())
+
+using Printf
+using LinearAlgebra
+using SparseArrays
+using AlgebraicMultigrid
+using Random
+
+using MyModule
+
+using FFTW
+using SpecialFunctions: erf
+include("../common/potential_H_atom.jl")
+
+function main()
+
+    Random.seed!(1234)
+
+    AA = [ 0.0,  0.0,  0.0]
+    BB = [16.0, 16.0, 16.0]
+    NN = [41, 41, 41]
+
+    grid = FD3dGrid( NN, AA, BB, pbc=(true,true,true) )
+    println(grid)
+
+    atoms = Atoms( xyz_string=
+        """
+        1
+
+        H  8.0  8.0  8.0
+        """, in_bohr=true, LatVecs=16.0*diagm(ones(3)), pbc=(true,true,true) )
+    
+    gvec = GVectors(grid)
+    #V_Ps_loc = pot_H_atom_G(atoms, grid, gvec)
+    V_Ps_loc = pot_Hps_GTH_G(atoms, grid, gvec)
+
+    Nstates = 1
+    Nelectrons = 1
+
+    Ham = Hamiltonian(atoms, grid, V_Ps_loc, Nelectrons=1)
+
+    Npoints = grid.Npoints
+    dVol = grid.dVol
+
+    psi = rand(Float64,Npoints,Nstates)
+    ortho_sqrt!(psi)
+    psi = psi/sqrt(dVol)
+
+    println("Test normalization")
+    for i in 1:Nstates
+        @printf("%18.10f\n", dot(psi[:,i], psi[:,i])*dVol )
+    end
+
+    Rhoe = calc_rhoe( Ham, psi )
+    @printf("Integrated Rhoe = %18.10f\n", sum(Rhoe)*dVol)
+
+    update!( Ham, Rhoe )
+
+    evals = zeros(Float64,Nstates)
+    Etot_old = 0.0
+    dEtot = 0.0
+    betamix = 0.5
+    dRhoe = 0.0
+    NiterMax = 100
+
+    Ham.energies.NN = calc_E_NN( atoms.LatVecs, atoms, [1.0] )
+
+    for iterSCF in 1:NiterMax
+
+        evals = diag_LOBPCG!( Ham, psi, Ham.precKin, verbose_last=false )
+        psi = psi/sqrt(dVol)
+
+        Rhoe_new = calc_rhoe( Ham, psi )
+        Rhoe = betamix*Rhoe_new + (1-betamix)*Rhoe
+
+        update!( Ham, Rhoe )
+
+        Etot = sum( calc_energies( Ham, psi ) )
+
+        dRhoe = sum(abs.(Rhoe - Rhoe_new))/Npoints # MAE
+        dEtot = abs(Etot - Etot_old)
+
+        @printf("%5d %18.10f %18.10e %18.10e\n", iterSCF, Etot, dEtot, dRhoe)
+
+        if dEtot < 1e-6
+            @printf("Convergence is achieved in %d iterations\n", iterSCF)
+            for i in 1:Nstates
+                @printf("%3d %18.10f\n", i, evals[i])
+            end
+            break
+        end
+
+        Etot_old = Etot
+    end
+
+    println(Ham.energies)
+
+end
+
+main()
