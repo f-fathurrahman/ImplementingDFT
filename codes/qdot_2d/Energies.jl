b3770f2c1a80d6e6e14df8c18db925f7c27fad17
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-07-14 17:52:18 +0700

    Remove NN field from Energies for qdot_2d

--- codes/qdot_2d/Energies.jl
+++ codes/qdot_2d/Energies.jl
@@ -3,16 +3,17 @@ mutable struct Energies
     Ps_loc::Float64
     Hartree::Float64
     XC::Float64
-    NN::Float64
 end
+# We assume that E_NN is not relevant for 2d case
+
 
 function Energies()
-    return Energies(0.0, 0.0, 0.0, 0.0, 0.0)
+    return Energies(0.0, 0.0, 0.0, 0.0)
 end
 
 import Base: sum
 function sum( ene::Energies )
-    return ene.Kinetic + ene.Ps_loc + ene.Hartree + ene.XC + ene.NN
+    return ene.Kinetic + ene.Ps_loc + ene.Hartree + ene.XC
 end
 
 import Base: println
@@ -25,7 +26,6 @@ function println( ene::Energies )
     @printf("Ps_loc  = %18.10f\n", ene.Ps_loc)
     @printf("Hartree = %18.10f\n", ene.Hartree)
     @printf("XC      = %18.10f\n", ene.XC)
-    @printf("NN      = %18.10f\n", ene.NN)
     @printf("----------------------------\n")
     @printf("Total   = %18.10f\n", sum(ene))
 end
