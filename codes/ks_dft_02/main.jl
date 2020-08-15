e07e2871e1ab87216dc35233e26748b7eba9eddd
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-08-15 14:23:47 +0700

    Timing KS_solve

--- codes/ks_dft_02/main.jl
+++ codes/ks_dft_02/main.jl
@@ -21,9 +21,9 @@ function main(Ham_func, N, grid_type)
 
     Ham = Ham_func(N, grid_type=grid_type)
 
-    println(Ham.atoms)
-    println(Ham.grid)
-    check_betaNL_norm(Ham.grid, Ham.pspotNL)
+    #println(Ham.atoms)
+    #println(Ham.grid)
+    #check_betaNL_norm(Ham.grid, Ham.pspotNL)
 
     @printf("\nsizeof Ham  = %18.10f MiB\n", Base.summarysize(Ham)/1024/1024)
 
@@ -34,9 +34,9 @@ function main(Ham_func, N, grid_type)
     psi = rand(Float64,Nbasis,Nstates)
     ortho_sqrt!(psi,dVol)
 
-    #KS_solve_Emin_PCG!(Ham, psi)
+    KS_solve_Emin_PCG!(Ham, psi)
     #KS_solve_SCF!(Ham, psi, betamix=0.25)
-    KS_solve_SCF_potmix!(Ham, psi, betamix=0.25)
+    #KS_solve_SCF_potmix!(Ham, psi, betamix=0.25)
 end
 
-main(create_Ham_CO, 40, :FD)
+@time main(create_Ham_CO, 40, :FD)
