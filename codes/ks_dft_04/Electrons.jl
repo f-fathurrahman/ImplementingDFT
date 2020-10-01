c0bafa4041db8a6cf82d775ac986e41fc64a82a2
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-10-02 03:34:53 +0700

    Update for Nspin=2, change kwarg Nstates_empty to Nstates_extra

--- codes/ks_dft_04/Electrons.jl
+++ codes/ks_dft_04/Electrons.jl
@@ -1,45 +1,58 @@
 mutable struct Electrons
+    Nspin::Int64
     Nelectrons::Int64
     Nstates::Int64
     Nstates_occ::Int64
-    Focc::Array{Float64,1}
-    eorbs::Array{Float64,1}
+    Focc::Array{Float64,2}
+    eorbs::Array{Float64,2}
 end
 
 
 function Electrons(
     atoms::Atoms, pspots::Array{PsPot_GTH,1};
-    Nstates=nothing, Nstates_empty=0
+    Nstates=nothing, Nstates_extra=0, Nspin=1
 )
+
+    @assert Nspin <= 2
 
     Nelectrons = get_Nelectrons(atoms,pspots)
 
     is_odd = round(Int64,Nelectrons)%2 == 1
 
-    # If Nstates is not specified and Nstates_empty == 0, we calculate
+    # If Nstates is not specified and Nstates_extra == 0, we calculate
     # Nstates manually from Nelectrons
     if (Nstates == nothing)
         Nstates = round( Int64, Nelectrons/2 )
         if Nstates*2 < Nelectrons
             Nstates = Nstates + 1
         end
-        if Nstates_empty > 0
-            Nstates = Nstates + Nstates_empty
+        if Nstates_extra > 0
+            Nstates = Nstates + Nstates_extra
         end
     end
 
-    Focc = zeros(Float64,Nstates)
-    eorbs = zeros(Float64,Nstates)
+    Focc = zeros(Float64,Nstates,Nspin)
+    eorbs = zeros(Float64,Nstates,Nspin)
     
-    Nstates_occ = Nstates - Nstates_empty
+    Nstates_occ = Nstates - Nstates_extra
     
-    for ist = 1:Nstates_occ-1
-        Focc[ist] = 2.0
-    end
-    if is_odd
-        Focc[Nstates_occ] = 1.0
-    else
-        Focc[Nstates_occ] = 2.0
+    if Nspin == 1
+        for ist = 1:Nstates_occ-1
+            Focc[ist,1] = 2.0
+        end
+        if is_odd
+            Focc[Nstates_occ,1] = 1.0
+        else
+            Focc[Nstates_occ,1] = 2.0
+        end
+    elseif Nspin == 2
+        for ist = 1:Nstates_occ-1
+            Focc[ist,1] = 1.0
+            Focc[ist,2] = 1.0
+        end
+        if is_odd
+            Focc[Nstates_occ,1] = 1.0
+        end
     end
 
     sFocc = sum(Focc)
@@ -49,5 +62,5 @@ function Electrons(
         error(@sprintf("ERROR: diff sum(Focc) and Nelectrons is not small\n"))
     end
 
-    return Electrons( Nelectrons, Nstates, Nstates_occ, Focc, eorbs )
+    return Electrons( Nspin, Nelectrons, Nstates, Nstates_occ, Focc, eorbs )
 end
