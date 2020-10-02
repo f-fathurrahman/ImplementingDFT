dc0574d0df7c5252b0c375527d8055dbc4bfd903
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-10-02 08:45:09 +0700

    Pretty printing for Electrons

--- codes/ks_dft_04/Electrons.jl
+++ codes/ks_dft_04/Electrons.jl
@@ -64,3 +64,83 @@ function Electrons(
 
     return Electrons( Nspin, Nelectrons, Nstates, Nstates_occ, Focc, eorbs )
 end
+
+import Base: show
+function show( io::IO, electrons::Electrons; header=true, all_states=false )
+
+    Nspin = electrons.Nspin
+    Focc = electrons.Focc
+    Nelectrons = electrons.Nelectrons
+    Nstates = electrons.Nstates
+    Nstates_occ = electrons.Nstates_occ
+    Nspin = size(Focc,2)
+
+    if header
+        @printf(io, "\n")
+        @printf(io, "                                    ---------\n")
+        @printf(io, "                                    Electrons\n")
+        @printf(io, "                                    ---------\n")
+        @printf(io, "\n")
+    end
+    @printf(io, "Nspin         = %8d\n", Nspin)
+    @printf(io, "Nelectrons    = %8d\n", Nelectrons)
+    @printf(io, "Nstates       = %8d\n", Nstates)
+    @printf(io, "Nstates_occ   = %8d\n", Nstates_occ)
+    @printf(io, "Nstates_empty = %8d\n\n", Nstates - Nstates_occ)
+
+    if Nspin == 1
+        @printf(io, "Occupation numbers: (spin-paired)\n\n")
+    else
+        @printf(io, "Occupation numbers: (spin-polarized)\n\n")
+    end
+
+    if Nstates < 8
+        all_states = true
+    end
+
+    Nk_per_line = 8
+
+    if all_states
+        for ist = 1:Nstates
+            @printf(io, "state #%4d = ", ist)
+            for ispin in 1:Nspin
+                @printf(io, "%8.5f ", Focc[ist,ispin])
+                if (ispin % Nk_per_line) == 0
+                    @printf(io, "\n")
+                    @printf(io, "              ")
+                end
+            end
+            #
+            @printf(io, "\n")
+        end
+    else
+        for ist = 1:4
+            @printf(io, "state #%4d = ", ist)
+            for ispin in 1:Nspin
+                @printf(io, "%8.5f ", Focc[ist,ispin])
+                if (ispin % Nk_per_line) == 0
+                    @printf(io, "\n")
+                    @printf(io, "              ")
+                end
+            end
+            @printf(io, "\n")
+        end
+        @printf(io, ".....\n\n")
+        #
+        for ist in Nstates-3:Nstates
+            @printf(io, "state #%4d = ", ist)
+            for ispin in 1:Nkspin
+                @printf(io, "%8.5f ", Focc[ist,ispin])
+                if (ispin % Nk_per_line) == 0
+                    @printf(io, "\n")
+                    @printf(io, "              ")
+                end
+            end
+            @printf(io, "\n")
+        end
+    end
+end
+
+function show( electrons::Electrons; header=true, all_states=false )
+    show( stdout, electrons, header=header, all_states=all_states )
+end
