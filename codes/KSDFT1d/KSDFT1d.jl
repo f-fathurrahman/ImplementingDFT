f0b3bd454e38e089ea46fce0a6a1372e1d269667
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-12-26 16:14:31 +0700

    Add some constants, export calc_E_NN

--- codes/KSDFT1d/KSDFT1d.jl
+++ codes/KSDFT1d/KSDFT1d.jl
@@ -3,6 +3,21 @@ module KSDFT1d
 using Printf
 using LinearAlgebra
 
+# constants
+#
+# CODATA: https://physics.nist.gov/cgi-bin/cuu/Value?rydhcev
+const Ry2eV = 13.605693009  # Ry to eV
+const Ha2eV = 2*Ry2eV
+const eV2Ha = 1/Ha2eV
+
+#
+# CODATA: https://physics.nist.gov/cgi-bin/cuu/Value?bohrrada0
+# 1/bohr
+const ANG2BOHR = 1.8897261254578281  # angstrom to bohr
+const BOHR2ANG = 1/ANG2BOHR
+
+export Ry2eV, Ha2eV, eV2Ha, ANG2BOHR, BOHR2ANG
+
 include("INC_sch_1d.jl")
 
 include("FD1dGrid.jl")
@@ -33,7 +48,7 @@ include("calc_rhoe.jl")
 export calc_rhoe!
 
 include("calc_energies.jl")
-export calc_E_kin, calc_E_nn
+export calc_E_kin, calc_E_NN
 
 include("Libxc_old.jl")
 include("LDA_libxc_1d.jl")
