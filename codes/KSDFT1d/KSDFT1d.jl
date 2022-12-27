4341eb58f46008173fad3a819ababd3ce4439a0f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-12-27 20:09:52 +0700

    Using common/constants.jl and add more constants

--- codes/KSDFT1d/KSDFT1d.jl
+++ codes/KSDFT1d/KSDFT1d.jl
@@ -3,18 +3,7 @@ module KSDFT1d
 using Printf
 using LinearAlgebra
 
-# constants
-#
-# CODATA: https://physics.nist.gov/cgi-bin/cuu/Value?rydhcev
-const Ry2eV = 13.605693009  # Ry to eV
-const Ha2eV = 2*Ry2eV
-const eV2Ha = 1/Ha2eV
-
-#
-# CODATA: https://physics.nist.gov/cgi-bin/cuu/Value?bohrrada0
-# 1/bohr
-const ANG2BOHR = 1.8897261254578281  # angstrom to bohr
-const BOHR2ANG = 1/ANG2BOHR
+include("../common/constants.jl")
 
 export Ry2eV, Ha2eV, eV2Ha, ANG2BOHR, BOHR2ANG
 
