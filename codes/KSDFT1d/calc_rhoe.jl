75b89913804771986e8813cb68a061d71622f6cb
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-01-09 20:31:38 +0700

    Add non in-place version

--- codes/KSDFT1d/calc_rhoe.jl
+++ codes/KSDFT1d/calc_rhoe.jl
@@ -1,3 +1,12 @@
+function calc_rhoe( Ham::Hamiltonian1d, psi::Array{Float64,2} )
+    Npoints = size(psi, 1)
+    Nspin = 1 # XXX
+    rhoe = zeros(Float64,Npoints,Nspin)
+    calc_rhoe!(Ham, psi, rhoe)
+    return rhoe
+end
+
+# In-place version
 function calc_rhoe!(
     Ham::Hamiltonian1d,
     psi::Array{Float64,2},
