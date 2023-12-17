2df1f46097c666dbe4f333589daa477e78406189
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-12-18 05:24:14 +0700

    Add one problematic system

---
+++ codes/KSDFT1d/emin_smearing/system_defs_03.jl
@@ -0,0 +1,33 @@
+function create_atoms()
+    Natoms = 2
+    σ = ones(Float64, Natoms)*(1.0)
+    masses = ones(Float64, Natoms)*42000.0
+    Zvals = ones(Float64, Natoms)*5.0
+    L = 10.0
+    atpos = zeros(Float64, Natoms)
+    atpos[1] = -1.5
+    atpos[2] =  1.5
+    return Atoms1d( atpos, Zvals, σ, masses, L )
+end
+
+function pot_gaussian( x, x0 )
+    return -20.0*exp(-4.5*(x-x0)^2)
+end
+# too negative amplitude might gave NaN to g_Haux
+
+function init_Vions!(Ham)
+    atpos = Ham.atoms.positions
+    Natoms = Ham.atoms.Natoms
+    for ia in 1:Natoms
+        Ham.potentials.Ions[:] += pot_gaussian.(Ham.grid.x, atpos[ia])
+    end
+    return
+end
+
+function init_Hamiltonian()
+    atoms = create_atoms()
+    Ham = Hamiltonian1d(atoms, 61, Nstates_extra=6)
+    init_Vions!(Ham)
+    Ham.energies.NN = calc_E_NN(Ham.atoms) # also calculate E_NN
+    return Ham
+end
