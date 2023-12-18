94fc8d3c1df11990e947a4cd28f4530fefc77298
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-12-18 12:28:22 +0700

    Add function to generate random unitary matrix

--- codes/KSDFT1d/utilities.jl
+++ codes/KSDFT1d/utilities.jl
@@ -1,3 +1,15 @@
+# https://discourse.julialang.org/t/how-to-generate-a-random-unitary-matrix-perfectly-in-julia/34102
+function random_unitary_matrix(N::Int64)
+    #x = (rand(N,N) + rand(N,N)*im) / sqrt(2)
+    X = rand(Float64, N,N)
+    F = qr(X)
+    diagR = sign.(real(diag(F.R)))
+    diagR[diagR.==0] .= 1
+    diagRm = diagm(diagR)
+    U = F.Q * diagRm
+    return U
+end
+
 function transform_psi_Haux!(psi, Haux)
     λ, Urot = eigen(Hermitian(Haux))
     psi[:,:] = psi[:,:]*Urot
