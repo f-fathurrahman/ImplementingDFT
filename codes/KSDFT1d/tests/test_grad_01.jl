44af8944716b0ae196ae7fd4c1a4eab71dbceb41
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-12-18 10:05:58 +0700

    Revisit test_grad

--- codes/KSDFT1d/tests/test_grad_01.jl
+++ codes/KSDFT1d/tests/test_grad_01.jl
@@ -7,6 +7,7 @@ import Random
 
 include("system_defs_01.jl")
 include("../utilities.jl")
+include("direct_min_no_smearing.jl")
 
 Ham = init_Hamiltonian()
 
@@ -14,19 +15,23 @@ hx = Ham.grid.hx
 Npoints = Ham.grid.Npoints
 Nstates = Ham.electrons.Nstates
 
-psi = ortho_sqrt( rand(Float64, Npoints, Nstates) )
+psi = ortho_sqrt( randn(Float64, Npoints, Nstates) )
 psi[:] = psi[:]/sqrt(hx)
 E0 = calc_KohnSham_Etotal!(Ham, psi)
 g0 = calc_grad(Ham, psi)
 
-Δ = 1e-10
-dW = randn(Float64, Npoints, Nstates)
-psi1 = ortho_sqrt( psi + Δ*dW )
-psi1[:] = psi1[:]/sqrt(hx)
-E1 = calc_KohnSham_Etotal!(Ham, psi1)
+for i in 4:11
+    Δ = 10.0^(-i)
+    dW = randn(Float64, Npoints, Nstates)
+    psi1 = ortho_sqrt( psi + Δ*dW )
+    psi1[:] = psi1[:]/sqrt(hx)
+    E1 = calc_KohnSham_Etotal!(Ham, psi1)
+    dE = 2*real( sum( g0 .* (Δ*dW) ) )*hx
+    #
+    println("Δ = ", Δ)
+    println("dE = ", dE)
+    println("E1 - E0 = ", E1 - E0)
+    println("ratio (should be close to 1) = ", abs((E1 - E0)/dE))
+end
 
-dE = 2*real( sum( g0 .* (Δ*dW) ) )*hx
 
-println("dE = ", dE)
-println("E1 - E0 = ", E1 - E0)
-println("ratio (should be close to 1) = ", abs((E1 - E0)/dE))
